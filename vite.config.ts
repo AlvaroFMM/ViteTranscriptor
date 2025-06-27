5073e052d346c52a6d3f78547765d715c4134949
Alvaro Felix <system.h16@gmail.com>
2025-06-26 20:50:39 -0500

    Update base path in vite.config.js to /ViteTranscriptor/

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ** MODIFICA ESTA LÍNEA **
+      base: '/ViteTranscriptor/', // <--- Debe ser el nombre exacto de tu repositorio, rodeado de barras
+      // **********************
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
